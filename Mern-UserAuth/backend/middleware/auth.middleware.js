16664dfaf6cec990c46e951337b48091aeb42370
rajarya199 <aryalrajan078@gmail.com>
2025-11-25 21:09:18 +0545

    auth middleware

---
+++ Mern-UserAuth/backend/middleware/auth.middleware.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+export const authorizeUser= async (req, res, next) => {
+  
+    const authHeader = req.headers.authorization;
+
+    // Get the token  from the Authorization header if it starts with "Bearer "
+    const token = (authHeader && authHeader.toLowerCase().startsWith('bearer ') ? authHeader.split(' ')[1] : null);
+  if (!token)
+    return res.status(401).json({ message: "Not authorized, token missing" });
+
+  try {
+    //verify token with jwt secreet
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            // 4. Attach the decoded user information to the request object
+
+    req.user = await User.findById(decoded.id).select("-password");
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Token invalid or expired" });
+  }
+};
