19ff96fc5c628c4e9d10da7f9dcfe904fc0a3bf4
rajarya199 <aryalrajan078@gmail.com>
2025-11-25 21:38:25 +0545

    user provider wrap

--- Mern-UserAuth/client/src/App.jsx
+++ Mern-UserAuth/client/src/App.jsx
@@ -1,12 +1,12 @@
-
+import { UserProvider } from './context/user.provider'
 import './App.css'
 import AppRoutes from './routes/AppRoutes'
 function App() {
 
   return (
-    <>
+    <UserProvider>
   <AppRoutes/>
-    </>
+    </UserProvider>
   )
 }
 
