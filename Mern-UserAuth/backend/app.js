3487789804fff0fca70f6379344a36baed4d589d
rajarya199 <aryalrajan078@gmail.com>
2025-11-26 12:39:25 +0545

    app-

--- Mern-UserAuth/backend/app.js
+++ Mern-UserAuth/backend/app.js
@@ -1,6 +1,12 @@
 import express from "express";
 import authRoutes from './routes/auth.route.js'
+import cors from "cors";
+import bodyParser from 'body-parser';
+
 const app = express();
+app.use(cors());
+app.use(bodyParser.json({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.json());
 app.use("/api", authRoutes);
 
