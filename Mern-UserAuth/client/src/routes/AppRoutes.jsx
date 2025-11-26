3ad17abc0348d27e945a58b75ecfec23638ac42d
rajarya199 <aryalrajan078@gmail.com>
2025-11-26 22:27:13 +0545

    app routes--

--- Mern-UserAuth/client/src/routes/AppRoutes.jsx
+++ Mern-UserAuth/client/src/routes/AppRoutes.jsx
@@ -3,14 +3,18 @@ import { Route,BrowserRouter,Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import Register from '../pages/Register'
 import Login from '../pages/Login'
+import Layout from '../layout/Layout'
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
     <Routes>
-        <Route index element={<Home/>}/>
+      <Route path='/' element={<Layout/>}>
+       <Route index element={<Home/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path='/login' element={<Login/>}/>
+      </Route>
+       
     </Routes>
       
     </BrowserRouter>
