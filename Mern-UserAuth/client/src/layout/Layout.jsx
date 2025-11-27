12e7b37eca48157822f41f9d897ed5fd2c197dab
rajarya199 <aryalrajan078@gmail.com>
2025-11-27 21:36:39 +0545

    layout.

--- Mern-UserAuth/client/src/layout/Layout.jsx
+++ Mern-UserAuth/client/src/layout/Layout.jsx
@@ -3,12 +3,15 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 const Layout = () => {
   return (
-    <div className="w-full min-h-screen  bg-white">
-      <Navbar/>
-        <Outlet />
-        <Footer/>
-    </div>
+    <div className="flex flex-col min-h-screen bg-white">
+      <Navbar />
 
+      <main className="grow pt-16 md:pt-20">
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
   );
 };
 
