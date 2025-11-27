2a538394d055013950ef4fa43be2b16d094266e5
rajarya199 <aryalrajan078@gmail.com>
2025-11-27 21:37:21 +0545

    protect route

--- Mern-UserAuth/client/src/routes/AppRoutes.jsx
+++ Mern-UserAuth/client/src/routes/AppRoutes.jsx
@@ -4,7 +4,8 @@ import Home from '../pages/Home'
 import Register from '../pages/Register'
 import Login from '../pages/Login'
 import Layout from '../layout/Layout'
-
+import Profile from '../pages/Profile'
+import ProtectedRoute from './ProtectedRoute'
 const AppRoutes = () => {
   return (
     <BrowserRouter>
@@ -13,6 +14,7 @@ const AppRoutes = () => {
        <Route index element={<Home/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/profile' element={<ProtectedRoute><Profile/> </ProtectedRoute>}/>
       </Route>
        
     </Routes>
