144862d3813e51bf807b948dcd8270e390734b2a
rajarya199 <aryalrajan078@gmail.com>
2025-11-27 22:57:35 +0545

    profile route

--- Mern-UserAuth/backend/routes/auth.route.js
+++ Mern-UserAuth/backend/routes/auth.route.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { loginUser, registerUser } from "../controllers/auth.controller.js";
 const router = express.Router();
-
+import { authorizeUser } from "../middleware/auth.middleware.js";
+import { ProfileController } from "../controllers/user.controller.js";
 router.post('/register',registerUser)
 router.post('/login',loginUser)
+router.get('/profile',authorizeUser,ProfileController)
 
 export default router;
