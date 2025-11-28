32294332ec41606ed6db9eac89eb1ced50188f0e
rajarya199 <aryalrajan078@gmail.com>
2025-11-28 19:51:51 +0545

    protected route

---
+++ Mern-UserAuth/client/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { UserContext } from "../context/user.context";
+
+const ProtectedRoute = ({children}) => {
+    const{user,loading}=useContext(UserContext)
+
+  if (loading) return <div>Loading...</div>;
+  if (!user) return <Navigate to="/login" replace />;
+
+ return children
+}
+
+export default ProtectedRoute
