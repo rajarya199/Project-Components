d7ba642ae2a4169a46380fa6a1ce1e380eb840f9
rajarya199 <aryalrajan078@gmail.com>
2025-12-09 22:39:22 +0545

    constant-navheader item

---
+++ next_with_clerk_auth/src/constants/index.ts
@@ -0,0 +1,7 @@
+export const headerLinks=[
+    {label:'Home',route:'/'},
+    {label:'About',route:'/about'},
+    {label:'Services',route:'/services'},
+    {label:'Contact',route:'/contact'},
+]
+
