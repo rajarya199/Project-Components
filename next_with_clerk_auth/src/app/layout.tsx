4ca74307879f728432a0e903a730cffc7fa45785
rajarya199 <aryalrajan078@gmail.com>
2025-12-10 23:38:38 +0545

    layout

--- next_with_clerk_auth/src/app/layout.tsx
+++ next_with_clerk_auth/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
-
+import Navbar from "@/components/nav/Navbar";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,7 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="h-screen flex flex-col">
+<Navbar/>
+      <main className="flex-1">{children}</main>
+        </div>
+      
       </body>
     </html>
     </ClerkProvider>
