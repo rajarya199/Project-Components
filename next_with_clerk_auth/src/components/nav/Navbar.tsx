fa14fa60ab253fc8e9374d074d54cfe17831f3a7
rajarya199 <aryalrajan078@gmail.com>
2025-12-13 23:15:38 +0545

    sign in button

--- next_with_clerk_auth/src/components/nav/Navbar.tsx
+++ next_with_clerk_auth/src/components/nav/Navbar.tsx
@@ -19,7 +19,9 @@ const Navbar = () => {
   
         </nav>
         <SignedOut>
-        <SignInButton/>
+        <SignInButton>
+          <button className='px-4 py-2 bg-teal-600 text-white rounded-md hover:bg-teal-700 transition'>Sign In</button>
+          </SignInButton>
         </SignedOut>
         <SignedIn>
           <UserButton/>
