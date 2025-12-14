4d05f57e0124b550bc1fbc2e223c403b6041ede4
rajarya199 <aryalrajan078@gmail.com>
2025-12-14 22:41:46 +0545

    contact page

---
+++ next_with_clerk_auth/src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className='wrapper min-h-screen flex flex-col p-8 bg-linear-to-br from-blue-50 to-indigo-100'>
+      this is contact us page
+    </main>
+  )
+}
+
+export default page
