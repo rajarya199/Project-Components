71256cf9cd85a9216d3b45e8166035a210cb3443
rajarya199 <aryalrajan078@gmail.com>
2025-12-15 21:01:07 +0545

    user model

---
+++ next_with_clerk_auth/src/model/user.model.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+  clerkId: { type: String, required: true, unique: true },
+  email: { type: String, required: true },
+  name: String,
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
+  createdAt: { type: Date, default: Date.now }
+}); 
+
+export const UserModel = mongoose.models.User || mongoose.model('User', userSchema);
+ 
