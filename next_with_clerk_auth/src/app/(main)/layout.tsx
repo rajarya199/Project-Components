95a8ef0273b394875401d1bfb98381436eccbdc5
rajarya199 <aryalrajan078@gmail.com>
2025-12-17 00:31:12 +0545

    layout

---
+++ next_with_clerk_auth/src/app/(main)/layout.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@/components/nav/Navbar";
+
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="flex h-screen flex-col">
+      <Navbar/>
+      <main className="flex-1">{children}</main>
+    </div>
+  )
+}
