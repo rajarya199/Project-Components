05818f196e5696bfde808ef8519e56bd4425560b
rajarya199 <aryalrajan078@gmail.com>
2025-12-18 20:26:08 +0545

    crteate user

---
+++ next_with_clerk_auth/src/app/actions/users.action.ts
@@ -0,0 +1,25 @@
+"use server"
+import { revalidatePath } from 'next/cache'
+import type { UserJSON } from '@clerk/nextjs/server'
+
+import { connectToDatabase } from '@/lib/dbConfig'
+import {User} from '@/model/user.model'
+export async function createUser(user: UserJSON) {
+try{
+      await connectToDatabase()
+   const existingUser = await User.findOne({ clerkId: user.id })
+    if (existingUser) return existingUser
+  const newUser = new User({
+    clerkId: user.id,
+    avatar:user.image_url,
+    email: user.email_addresses[0].email_address,
+    name: `${user.first_name} ${user.last_name}`,
+  })
+  await newUser.save()
+  revalidatePath('/')
+  return newUser
+}catch(error){
+      console.error('❌ Error creating user from Clerk:', error)
+    throw new Error('Failed to create user')
+}
+}
