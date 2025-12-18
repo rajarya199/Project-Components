4bc331435664a494fcd0dc0ab1e4672b406995a2
rajarya199 <aryalrajan078@gmail.com>
2025-12-18 20:26:39 +0545

    user model

--- next_with_clerk_auth/src/model/user.model.ts
+++ next_with_clerk_auth/src/model/user.model.ts
@@ -3,6 +3,7 @@ const userSchema = new mongoose.Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true },
   name: String,
+  avatar:String,
   role: { type: String, enum: ['user', 'admin'], default: 'user' },
   createdAt: { type: Date, default: Date.now }
 }); 
