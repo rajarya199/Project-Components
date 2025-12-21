0d73bba35cd37db56582f7cee1a3a6f97a8e8ae4
rajarya199 <aryalrajan078@gmail.com>
2025-12-21 15:20:56 +0545

    update user action

--- next_with_clerk_auth/src/app/actions/users.action.ts
+++ next_with_clerk_auth/src/app/actions/users.action.ts
@@ -7,18 +7,39 @@ export async function createUser(user: UserJSON) {
 try{
       await connectToDatabase()
    const existingUser = await User.findOne({ clerkId: user.id })
-    if (existingUser) return existingUser
-  const newUser = new User({
+    if (existingUser) {
+      throw new Error(`User already exists: ${existingUser._id}`)
+    }
+  const newUser =await User.create({
     clerkId: user.id,
     avatar:user.image_url,
     email: user.email_addresses[0].email_address,
     name: `${user.first_name} ${user.last_name}`,
   })
-  await newUser.save()
   revalidatePath('/')
   return newUser
 }catch(error){
       console.error('❌ Error creating user from Clerk:', error)
     throw new Error('Failed to create user')
 }
+}
+
+export async function updateUser(clerkId:string,user: UserJSON) {
+  try{
+await connectToDatabase()
+  const existingUser = await User.findOne({ clerkId: clerkId })
+    if (!existingUser) {
+      console.error(`User with clerkId ${clerkId} not found`);
+      throw new Error('User not found');
+    }
+    const updatedUser=await User.findOneAndUpdate({clerkId:clerkId},{
+       email: user.email_addresses[0]?.email_address,
+        name: `${user.first_name ?? ''} ${user.last_name ?? ''}`.trim(),
+        avatar: user.image_url,
+    }, { new: true })
+    return updatedUser
+  }
+  catch(error){
+    console.error('❌ Error updating user from Clerk:', error)
+  }
 }
