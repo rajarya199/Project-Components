e0070c6a203ec2e608783d5c5e54b1fd3d2b96a3
rajarya199 <aryalrajan078@gmail.com>
2025-12-21 15:20:32 +0545

    update the model

--- next_with_clerk_auth/src/model/user.model.ts
+++ next_with_clerk_auth/src/model/user.model.ts
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
-const userSchema = new mongoose.Schema({
-  clerkId: { type: String, required: true, unique: true },
-  email: { type: String, required: true },
-  name: String,
-  avatar:String,
-  role: { type: String, enum: ['user', 'admin'], default: 'user' },
-  createdAt: { type: Date, default: Date.now }
-}); 
+const userSchema = new mongoose.Schema(
+  {
+    clerkId: { type: String, required: true, unique: true, index: true },
+    email: { type: String, unique: true, required: true, index: true },
+    name: String,
+    avatar: String,
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.models.User || mongoose.model('User', userSchema);
- export default User;
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
