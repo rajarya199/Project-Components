79f22d0551c10a36ce81e34611c3dc0e76e95d72
rajarya199 <aryalrajan078@gmail.com>
2025-12-23 20:15:58 +0545

    image config

--- next_with_clerk_auth/next.config.ts
+++ next_with_clerk_auth/next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     images: {
-    domains: ["img.clerk.com"],
+    domains: ["img.clerk.com",
+       "cdn.dribbble.com"
+    ],
   },
 };
 
