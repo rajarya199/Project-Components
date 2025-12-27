796883b2b252191d01d866e8b552188688bc802f
rajarya199 <aryalrajan078@gmail.com>
2025-12-27 18:09:39 +0545

    delete user

---
+++ next_with_clerk_auth/src/app/api/admin/users/[id]/route.ts
@@ -0,0 +1,49 @@
+import { NextResponse } from "next/server";
+import User from "@/model/user.model";
+import { connectToDatabase } from "@/lib/dbConfig";
+import { auth,clerkClient } from "@clerk/nextjs/server";
+
+export async function DELETE(req:Request,{ params }: { params: { id: string } }){
+    try{
+        //auth check
+          const { userId, sessionClaims } = await auth();
+
+    if (!userId) {
+      return NextResponse.json(
+        { success: false, error: "Unauthorized" },
+        { status: 401 }
+      );
+    }
+
+    // 2️⃣ Role check
+    const userRole = sessionClaims?.userRole as string | undefined;
+
+    if (userRole !== "admin") {
+      return NextResponse.json(
+        { success: false, error: "Admin access required" },
+        { status: 403 }
+      );
+    }
+      await connectToDatabase()
+    const user = await User.findById(params.id)
+    
+    if (!user) {
+      return NextResponse.json({ success: false, error: "User not found" }, { status: 404 })
+    }
+  // 1️⃣ Delete from Clerk
+  const client = await clerkClient()
+  await client.users.deleteUser(user.clerkId)
+
+  //delete from db
+    await User.findByIdAndDelete(params.id)
+return NextResponse.json({success:true},{status:200})
+
+    }
+    catch(error){
+        console.error("❌ Error deleting user :", error);
+  return NextResponse.json(
+          { success: false, error: "Failed to delete user" },
+          { status: 500 }
+        );
+    }
+}
