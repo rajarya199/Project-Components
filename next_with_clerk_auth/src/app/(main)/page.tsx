c0ab1b91ea5f5d1f23de3c66608cf4d9fe7aa29d
rajarya199 <aryalrajan078@gmail.com>
2025-12-30 21:37:38 +0545

    home update

--- next_with_clerk_auth/src/app/(main)/page.tsx
+++ next_with_clerk_auth/src/app/(main)/page.tsx
@@ -1,14 +1,15 @@
-
+"use client"
 import { 
   UserButton, 
   SignedIn, 
   SignedOut, 
-  SignInButton, 
-  SignUpButton,
+  useUser
 } from '@clerk/nextjs';
 import Link from 'next/link';
 
 export default function Home() {
+    const { user } = useUser(); 
+    const userRole = user?.publicMetadata?.userRole as string | undefined;
   return (
     <main className="min-h-screen flex flex-col items-center justify-center p-8 bg-linear-to-br from-blue-50 to-indigo-100">
       <div className="max-w-md w-full bg-white rounded-2xl shadow-xl p-8 space-y-6">
@@ -42,7 +43,7 @@ export default function Home() {
             
             {/* Social Login Buttons */}
             <div className="pt-4 space-y-2">
-              <SignInButton mode="redirect" forceRedirectUrl="/dashboard">
+              <Link href='/sign-in'>
                 <div className="w-full flex items-center justify-center gap-3 px-6 py-3 border border-gray-200 rounded-xl hover:bg-gray-50 transition-all">
                   <svg className="w-5 h-5" viewBox="0 0 24 24">
                     <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/>
@@ -52,16 +53,16 @@ export default function Home() {
                   </svg>
                   Continue with Google
                 </div>
-              </SignInButton>
+              </Link>
               
-              <SignInButton mode="redirect" forceRedirectUrl="/dashboard">
+              <Link href='/sign-in'>
                 <div className="w-full flex items-center justify-center gap-3 px-6 py-3 border border-gray-200 rounded-xl hover:bg-gray-50 transition-all">
                   <svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor">
                     <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
                   </svg>
                   Continue with GitHub
                 </div>
-              </SignInButton>
+              </Link>
             </div>
           </div>
         </SignedOut>
@@ -69,12 +70,23 @@ export default function Home() {
         <SignedIn>
           <div className="text-center">
             <UserButton afterSignOutUrl="/" />
-            <Link 
+
+            {userRole==="admin"?(
+                <Link 
               href="/dashboard" 
               className="w-full block mt-4 bg-blue-600 text-white py-3 rounded-xl hover:bg-blue-700 transition-all font-medium"
             >
               Go to Dashboard
             </Link>
+            ) :(
+                <Link 
+              href="/profile" 
+              className="w-full block mt-4 bg-blue-600 text-white py-3 rounded-xl hover:bg-blue-700 transition-all font-medium"
+            >
+              Go to Profile
+            </Link>
+            )}
+         
           </div>
         </SignedIn>
       </div>
